cfab7ea512c59a76f22769e08ba1fa598c2155e9
Alexandr-Jellyfish <tvodr.alexan@gmail.com>
2022-08-16 00:21:03 +0300

    practice_2

--- Practice/script.js
+++ Practice/script.js
@@ -10,12 +10,46 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
+if (personalMovieDB.count < 10) {
+    console.log('Просмотрено довольно мало фильмов');
+    alert('Просмотрено довольно мало фильмов');
+} else if ((personalMovieDB.count >= 10) && (personalMovieDB.count < 30)) {
+    console.log('Вы классический зритель');
+    alert('Вы классический зритель');
+} else if (personalMovieDB.count >= 30) {
+    console.log('Вы киноман');
+    alert('Вы киноман');
+} else {
+    console.log('Произошла ошибка');
+    alert('Произошла ошибка');
+}
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i = 0; i < personalMovieDB.count; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
+
+    if ((a != '') && (b != '') && (a != null) && (b != null) && (a.length <= 50)) {
+        personalMovieDB.movies[a] = b;
+    } else {
+        console.log('error');
+        i--;
+    }
+}
+
+let j = 0;
+while (j < personalMovieDB.count) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
+    personalMovieDB.movies[a] = b;
+    i++;
+}
+
+let k = 1;
+do {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
+    personalMovieDB.movies[a] = b; 
+    k++;
+} while (k < personalMovieDB.count);
 
 console.log(personalMovieDB);
